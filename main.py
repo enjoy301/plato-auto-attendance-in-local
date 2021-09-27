3976c8d9e0354e9a380b40ebd01759adfc6028d5
Eunjong-King <eunjong147@pusan.ac.kr>
2021-09-27 17:27:17 +0900

    REFACTOR logic

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
-from datetime import datetime, timedelta
+from datetime import datetime
 import time, os , json
+import logging
+
+logger = logging.getLogger()
 
 time_now = datetime.now()
 
+# 크롬드라이버 옵션 설정
 options = webdriver.ChromeOptions()
 options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
@@ -13,132 +17,146 @@ options.add_argument("--headless")
 options.add_argument("--mute-audio")
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-driver = webdriver.Chrome(options=options)
-driver.implicitly_wait(3)
-driver.get('https://plato.pusan.ac.kr/')
-
+# json파일 읽어와서 학번 비번 얻기
 with open(os.path.join("./", 'secrets.json'), encoding='UTF8') as f:
     secrets = json.loads(f.read())
-driver.find_element_by_id("input-username").send_keys(secrets["학번"])
-driver.find_element_by_id("input-password").send_keys(secrets["비밀번호"])
-driver.find_element_by_xpath('//*[@id="page-header"]/div[1]/div/div[2]/form/div/input[3]').click()
 
-time.sleep(2)
+driver = webdriver.Chrome(options=options)
+driver.implicitly_wait(3)
 
-main_page = driver.find_element_by_xpath('//*[@id="page-container"]')
-notice_list = main_page.find_elements_by_css_selector('[class|="modal notice_popup ui-draggable"]')
-z_index_list = []
-for i in notice_list:
-    z_index_list.append(i.value_of_css_property("z-index"))
+try:
+    driver.get('https://plato.pusan.ac.kr/')
+    driver.find_element_by_id("input-username").send_keys(secrets["학번"])
+    driver.find_element_by_id("input-password").send_keys(secrets["비밀번호"])
+    driver.find_element_by_xpath('//*[@id="page-header"]/div[1]/div/div[2]/form/div/input[3]').click()
 
-for i in range(len(notice_list)):
-    max_index = z_index_list.index(max(z_index_list))
-    notice_list[max_index].find_element_by_class_name('close_notice').click()
-    z_index_list.pop(max_index)
-    notice_list.pop(max_index)
-    driver.execute_script("window.scrollTo(0, 0)") 
+    time.sleep(2)
 
-main_page = driver.find_element_by_xpath('//*[@id="page-content"]/div/div[1]/div[2]/ul')
-for i in range(1, 100):
-    xpath = f'./li[{i}]'
+    # POPUP 제거 부분
     try:
-        main_page.find_element_by_xpath(xpath)
-    except:
-        count_lecture = i - 1
-        break
+        main_page = driver.find_element_by_xpath('//*[@id="page-container"]')
+        notice_list = main_page.find_elements_by_css_selector('[class|="modal notice_popup ui-draggable"]')
+        z_index_list = []
+        for i in notice_list:
+            z_index_list.append(i.value_of_css_property("z-index"))
 
+        for i in range(len(notice_list)):
+            max_index = z_index_list.index(max(z_index_list))
+            notice_list[max_index].find_element_by_class_name('close_notice').click()
+            z_index_list.pop(max_index)
+            notice_list.pop(max_index)
+            driver.execute_script("window.scrollTo(0, 0)")
+    except Exception as e:
+        print("POPUP 오류")
+        logger.exception(str(e))
 
-total_lecture_list = []
-card_body = driver.find_element_by_xpath('//*[@id="mCSB_2_container"]/div/div[1]/div[2]')
+    # 강의 개수 세기
+    count_lecture = len(driver.find_elements_by_xpath('//*[@id="page-content"]/div/div[1]/div[2]/ul/li'))
 
-# for i in range(1, count_lecture+1)
-for i in range(1, count_lecture+1):
-    main_page = driver.find_element_by_xpath('//*[@id="page-content"]/div/div[1]/div[2]/ul')
-    main_page.find_element_by_xpath(f'./li[{i}]/div/a/div').click()
-    time.sleep(1)
-    url = "https://plato.pusan.ac.kr/report/ubcompletion/user_progress_a.php?id="+str(driver.current_url[-6:])
-    driver.execute_script(f'window.open("{url}");')
-    driver.switch_to.window(driver.window_handles[1])
-    time.sleep(0.5)
-    main_table = driver.find_element_by_xpath('//*[@id="ubcompletion-progress-wrapper"]/div[2]/table/tbody')
-    tr_list = main_table.find_elements_by_tag_name('tr')
-    lecture_list = []
-    for lec in tr_list:
-        td_list = lec.find_elements_by_tag_name('td')
-        if len(td_list) == 4:
-            if td_list[3].text == "X":
-                lecture_list.append(td_list[0].text)
-                
-        else:
-            if td_list[4].text == "X":
-                lecture_list.append(td_list[1].text)
+    for i in range(1, count_lecture+1):
+        main_page = driver.find_element_by_xpath('//*[@id="page-content"]/div/div[1]/div[2]/ul')
+        main_page.find_element_by_xpath(f'./li[{i}]/div/a/div').click()
+        time.sleep(1)
+        url = "https://plato.pusan.ac.kr/report/ubcompletion/user_progress_a.php?id="+str(driver.current_url[-6:])
+        driver.execute_script(f'window.open("{url}");')
+        driver.switch_to.window(driver.window_handles[1])
+        time.sleep(0.5)
+        main_table = driver.find_element_by_xpath('//*[@id="ubcompletion-progress-wrapper"]/div[2]/table/tbody')
+        tr_list = main_table.find_elements_by_tag_name('tr')
+        lecture_list = []
+        for lec in tr_list:
+            td_list = lec.find_elements_by_tag_name('td')
+            if len(td_list) == 4:
+                if td_list[3].text == "O":
+                    lecture_list.append(td_list[0].text)
+                    
+            else:
+                if td_list[4].text == "O":
+                    lecture_list.append(td_list[1].text)
 
-    total_lecture_list.append(lecture_list)
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
-    driver.get('https://plato.pusan.ac.kr/')
-    time.sleep(1)
+        time.sleep(0.5)
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+        time.sleep(0.5)
 
-for i in range(1, count_lecture+1):
-    if len(total_lecture_list[i-1]) == 0:
-        continue
-    main_page = driver.find_element_by_xpath('//*[@id="page-content"]/div/div[1]/div[2]/ul')
-    main_page.find_element_by_xpath(f'./li[{i}]/div/a/div').click()
-    weeks_list = driver.find_element_by_xpath('//*[@id="course-all-sections"]/div/ul')
-    li_list = weeks_list.find_elements_by_tag_name('li')
-    for index, li in enumerate(li_list):
-        num = index + 1
-        ul_list = li.find_element_by_xpath(f'//*[@id="section-{num}"]/div[3]').find_elements_by_tag_name('ul')
-        if len(ul_list) == 0:
-            break
-        for file in ul_list[0].find_elements_by_tag_name('li'):
-            my_text = file.text
-            if (total_lecture_list[i-1][0] != 0) and (total_lecture_list[i-1][0] in my_text):
-                duration = my_text[my_text.find('동영상')+5:my_text.find('동영상')+46]
-                from_d = datetime.strptime(duration[:10], "%Y-%m-%d")
-                until_d = datetime.strptime(duration[22:-9], "%Y-%m-%d") + timedelta(days=1)
-                if (from_d <= time_now) and (time_now <= until_d):            
-                    print(total_lecture_list[i-1][0], "듣는 중...")
-                    raw_url = file.find_element_by_tag_name('a').get_attribute('onclick')
-                    url = raw_url[raw_url.find("https"):raw_url.find("', '', '")]
-                    driver.execute_script(f'window.open("{url}");')
-                    driver.switch_to.window(driver.window_handles[1])
+        if len(lecture_list) == 0:
+            driver.get('https://plato.pusan.ac.kr/')
+            time.sleep(1)
+            continue
+
+        now_index = 0
+        # li_list는 1주차부터 16주차까지 block들이 담겨있는 list
+        li_list = driver.find_element_by_xpath('//*[@id="course-all-sections"]/div/ul').find_elements_by_tag_name('li')
+        for index, li in enumerate(li_list):
+            num = index + 1
+            ul_list = li.find_element_by_xpath(f'//*[@id="section-{num}"]/div[3]').find_elements_by_tag_name('ul')
+            # "len(ul_list) == 0" 은 그 주차에 아무 강의 자료 없다는 뜻 
+            if len(ul_list) == 0:
+                continue
+            # file 하나하나 탐색
+            for file in ul_list[0].find_elements_by_tag_name('li'):
+                my_text = file.text
+                if ((lecture_list[now_index] in my_text) and ("동영상" in my_text)):
+                    duration = my_text[my_text.find('동영상')+5:my_text.find('동영상')+46]
+                    from_d = datetime.strptime(duration[:19], "%Y-%m-%d %H:%M:%S")
+                    until_d = datetime.strptime(duration[22:], "%Y-%m-%d %H:%M:%S")
                     try:
-                        driver.switch_to.alert.accept()
+                        # 출석인정기간인 경우
+                        if (from_d <= time_now) and (time_now <= until_d):
+                            print(lecture_list[now_index], "듣는 중...")
+                            raw_url = file.find_element_by_tag_name('a').get_attribute('onclick')
+                            url = raw_url[raw_url.find("https"):raw_url.find("', '', '")]
+                            driver.execute_script(f'window.open("{url}");')
+                            driver.switch_to.window(driver.window_handles[1])
+                            try:
+                                driver.switch_to.alert.accept()
+                            except:
+                                pass
+                            temp = driver.page_source
+                            try:
+                                driver.switch_to.alert.accept()
+                            except:
+                                pass
+                            driver.find_element_by_xpath('//*[@id="vod_viewer"]').click()
+                            time.sleep(5)
+                            # while True:
+                            #     left_hour = driver.find_element_by_xpath('//*[@id="my-video"]/div[4]/div[7]/div').text
+                            #     if "-0:00" == left_hour:
+                            #         break
+                            #     time.sleep(60)
+
+                            driver.find_element_by_class_name('vod_close_button').click()
+                            time.sleep(2)
+
+                            try:
+                                driver.switch_to.alert.accept()
+                            except:
+                                pass
+
+                            driver.switch_to.window(driver.window_handles[0])
+                            print(lecture_list[now_index], "듣기 완료")
+                            time.sleep(0.5)
                     except:
-                        pass
-                    temp = driver.page_source
-                    try:
-                        driver.switch_to.alert.accept()
-                    except:
-                        pass
-                    driver.find_element_by_xpath('//*[@id="vod_viewer"]').click()
-
-                    while True:
-                        left_hour = driver.find_element_by_xpath('//*[@id="my-video"]/div[4]/div[7]/div').text
-                        if "-0:00" == left_hour:
-                            break
-                        time.sleep(60)
-
-                    time.sleep(1)
-                    driver.find_element_by_class_name('vod_close_button').click()
-                    time.sleep(1)  # 동영상 재생 시간
-
-                    try:
-                        driver.switch_to.alert.accept()
-                    except:
-                        pass
-
-                    driver.switch_to.window(driver.window_handles[0])
-                    print(total_lecture_list[i-1][0], "듣기 완료")
-                if len(total_lecture_list[i-1]) == 1:
-                    total_lecture_list[i-1][0] = 0
+                        print(lecture_list[now_index], "재생 문제")
+                    now_index += 1
+                    if len(lecture_list) == now_index:
+                        # 이번 주차의 파일 하나 하나 탐색하는 for문을 break
+                        break
+                # 이번 파일이 동영상이 아니거나 찾고 있는 강의가 아닌 경우
                 else:
-                    total_lecture_list[i-1].pop(0)
-        if total_lecture_list[i-1][0] == 0:
-            break
+                    continue
+            if len(lecture_list) == now_index:
+                # 다음 주차의 블록으로 넘어가지 않는다는 break
+                now_index = 0
+                break
 
-    driver.get('https://plato.pusan.ac.kr/')
-    time.sleep(1)
+        driver.get('https://plato.pusan.ac.kr/')
+        time.sleep(1)
 
-driver.close()
+    driver.close()
+
+except Exception as e:
+    logger.exception('오류발생: ' + str(e))
+    for window in driver.window_handles:
+        driver.switch_to.window(window)
+        driver.close()
